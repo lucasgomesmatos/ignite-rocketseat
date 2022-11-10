0b42bead3cd352f0f1f5f31bc2e9145fbb7781d4
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-11-10 14:45:09 -0300

    ig news

---
+++ next/ignews/src/components/SingInButton/index.tsx
@@ -0,0 +1,20 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './style.module.scss';
+
+export function SignInButton() {
+  const isUserLoggerIn = true;
+
+  return isUserLoggerIn ? (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#04d361" />
+      Lucas Gomes
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button type="button" className={styles.signInButton}>
+      <FaGithub color="#eba417" />
+      Sign in with Github
+    </button>
+  );
+}
