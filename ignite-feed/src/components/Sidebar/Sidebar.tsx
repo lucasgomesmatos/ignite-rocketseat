a606484fe69ae6535478cd354dcbbe9eccd1ebd8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-13 22:45:12 -0300

    sidebar button

--- ignite-feed/src/components/Sidebar/Sidebar.tsx
+++ ignite-feed/src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export const Sidebar = () => {
   return (
@@ -8,11 +9,17 @@ export const Sidebar = () => {
         src="https://images.unsplash.com/photo-1611647832580-377268dba7cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/lucasgomesmatos.png"
+        />
         <strong>Lucas Gomes</strong>
         <span>Software Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
