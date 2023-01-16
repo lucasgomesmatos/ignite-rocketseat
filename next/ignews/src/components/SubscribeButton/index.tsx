ffe7392c7ea02385f84b43846a866ec60f631cb8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-16 13:28:24 -0300

    rolback

--- next/ignews/src/components/SubscribeButton/index.tsx
+++ next/ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../service/api';
+import { getStripeJs } from '../../service/stripe-js';
 import styles from './style.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,8 +8,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  async function handleSubribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    // criação do checkout session
+    try {
+      const response = await api.post('subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId });
+    } catch (e: any) {
+      alert(e.message);
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      onClick={handleSubribe}
+      className={styles.subscribeButton}
+    >
       Subscribe now
     </button>
   );
