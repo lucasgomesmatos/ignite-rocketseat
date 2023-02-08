2ed5beedb62244dd7635bcebdf5142ba263a7a8b
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-02-07 22:53:13 -0300

    typescript

--- ignite-feed/src/components/Comment/Comment.tsx
+++ ignite-feed/src/components/Comment/Comment.tsx
@@ -28,6 +28,7 @@ export const Comment = ({
     setLikeCount((old) => old + 1);
   }
 
+  console.log('first', author);
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src={author.avatarUrl} />
