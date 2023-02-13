a3181a40fca2bfd2d8cd32c047b43274556cce11
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-02-13 08:53:22 -0300

    form styles

--- ignite-timer/src/pages/Home/index.tsx
+++ ignite-timer/src/pages/Home/index.tsx
@@ -1,14 +1,56 @@
-import { HomeContainer } from './style';
+import { Play } from 'phosphor-react';
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from './style';
 
 export const Home = () => {
   return (
     <HomeContainer>
       <form>
-        <label htmlFor="task">Vou trabalhar em</label>
-        <input type="text" id="task" />
-        <label htmlFor="minutesAmount">durante</label>
-        <input type="text" id="minutesAmount" />
-        <span>minutos</span>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+        <StartCountDownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
       </form>
     </HomeContainer>
   );
