0511411b5377864e40fd8c5d9b719bb8f9bfcd76
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-03-01 09:05:28 -0300

    actions

--- ignite-timer/src/reducers/cycles/reducer.ts
+++ ignite-timer/src/reducers/cycles/reducer.ts
@@ -1,14 +1,9 @@
-import { Cycle } from '../@types/Cycle';
+import { Cycle } from '../../@types/Cycle';
+import { ActionTypes } from './actions';
 
 interface CyclesState {
   cycles: Cycle[];
   activeCyclesId: string | null;
-}
-
-export enum ActionTypes {
-  ADD_NEW_CYCLE = 'ADD_NEW_CYCLE',
-  INTERRUPT_CURRENT_CYCLE = 'INTERRUPT_CURRENT_CYCLE',
-  MARK_CURRENT_CYCLE_AS_FINISHED = 'MARK_CURRENT_CYCLE_AS_FINISHED',
 }
 
 export function cyclesReducer(state: CyclesState, action: any) {
