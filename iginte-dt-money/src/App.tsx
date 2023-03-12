c0c2909e13af2263154c8f36e54a6979b44d96cb
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-03-12 18:14:48 -0300

    start dt money

---
+++ iginte-dt-money/src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+};
