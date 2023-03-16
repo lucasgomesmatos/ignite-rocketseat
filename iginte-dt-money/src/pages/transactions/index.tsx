3d06e5a55845836e5ce4a8dbff30647f495513f8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-03-15 22:51:48 -0300

    table

--- iginte-dt-money/src/pages/transactions/index.tsx
+++ iginte-dt-money/src/pages/transactions/index.tsx
@@ -1,11 +1,38 @@
 import { Header } from '../../components/header';
 import { Summary } from '../../components/Summary';
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './style';
 
 export const Transactions = () => {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>{' '}
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome"> - R$ 120,00</PriceHighlight>{' '}
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 };
